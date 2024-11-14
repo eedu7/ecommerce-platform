48b83812a136bcf60a42ca220808a16513012d38
eedu7 <mueedahmad067@gmail.com>
2024-11-15 00:16:04 +0500

    MINOR: fixed the .env path issue

--- user-service/core/config.py
+++ user-service/core/config.py
@@ -1,14 +1,18 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
-class ConfigSettings(BaseSettings):
+class BaseConfig(BaseSettings):
     class Config:
         case_sensitive = True
-        env_file = "../.env"
+        env_file = Path(__file__).resolve().parent.parent / ".env"
         env_file_encoding = "utf-8"
 
 
-class Config(ConfigSettings):
+class AppConfig(BaseConfig):
+    """App-specific configuration settings."""
+
     POSTGRES_URL: str
     TEST_POSTGRES_URL: str
     JWT_SECRET_KEY: str
@@ -16,4 +20,4 @@ class Config(ConfigSettings):
     JWT_EXPIRE_MINUTES: int = 60 * 24
 
 
-config: Config = Config()
+config: AppConfig = AppConfig()
