7303c98859c3cf5633cbe7728822436b1986f347
eedu7 <mueedahmad067@gmail.com>
2024-11-14 22:35:01 +0500

    FEAT: Added the .env variable, updated the .env.example file  and config.py to access the env variables

---
+++ user-service/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+
+class ConfigSettings(BaseSettings):
+    class Config:
+        case_sensitive = True
+        env_file = "../.env"
+        env_file_encoding = "utf-8"
+
+
+class Config(ConfigSettings):
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_EXPIRE_MINUTES: int = 60 * 24
+
+
+config: Config = Config()
