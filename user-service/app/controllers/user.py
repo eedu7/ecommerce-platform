a1a6443abe607cec751584a85e3a4230c071eae7
eedu7 <mueedahmad067@gmail.com>
2024-11-22 10:35:48 +0500

    FEAT: Code refactored and added the update method, to both partial and fully update and also delete method in the user controller

--- user-service/app/controllers/user.py
+++ user-service/app/controllers/user.py
@@ -1,6 +1,11 @@
+from typing import Any, Dict
+
 from app.models import User
 from app.repositories import UserRepository
+from app.schemas.requests.profile import (ProfilePartialUpdateRequest,
+                                          ProfileUpdateRequest)
 from core.controller import BaseController
+from core.exceptions import BadRequestException, NotFoundException
 
 
 class UserController(BaseController[User]):
@@ -13,3 +18,31 @@ class UserController(BaseController[User]):
 
     async def get_by_email(self, email: str) -> User:
         return await self.user_repository.get_by_email(email)
+
+    async def update_user(
+        self,
+        user_id: int,
+        user_data: ProfileUpdateRequest | ProfilePartialUpdateRequest,
+        partial_update: bool = False,
+    ):
+        user: User = await self.get_by_id(user_id)
+        if not user:
+            raise NotFoundException(f"User not found. {e}")
+        if partial_update:
+            user_data: Dict[str, Any] = user_data.model_dump(exclude_none=True)
+        else:
+            user_data: Dict[str, Any] = user_data.model_dump(exclude_none=True)
+
+        try:
+            return await self.update_model(user, user_data)
+        except Exception as e:
+            raise BadRequestException(f"Error updating user. {e}")
+
+    async def delete_user(self, user_id: int):
+        user: User = await self.get_by_id(user_id)
+        if not user:
+            raise NotFoundException("User not found.")
+        try:
+            await self.delete(user)
+        except Exception as e:
+            raise BadRequestException(f"Error deleting user. {e}")
