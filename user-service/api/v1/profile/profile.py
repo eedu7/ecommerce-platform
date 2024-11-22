85e4bf5ad11b518de6b519dd85594cde1e124aae
eedu7 <mueedahmad067@gmail.com>
2024-11-22 10:36:28 +0500

    FEAT: updated the logic of profile api, for updating the user, both fully and partial

--- user-service/api/v1/profile/profile.py
+++ user-service/api/v1/profile/profile.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, Depends
 
+from app.controllers import UserController
 from app.models import User
+from app.schemas.requests.profile import (ProfilePartialUpdateRequest,
+                                          ProfileUpdateRequest)
 from app.schemas.responses.users import UserResponse, UserResponseDetail
+from core.factory import Factory
 from core.fastapi.dependencies import AuthenticationRequired, get_current_user
 
 profile_router: APIRouter = APIRouter()
@@ -15,10 +19,20 @@ def get_user(
 
 
 @profile_router.put("/")
-async def update_profile(user: User = Depends(get_current_user)):
-    return {"message": "User updated", "user": user}
+async def update_profile(
+    user_data: ProfileUpdateRequest,
+    current_user: User = Depends(get_current_user),
+    user_controller: UserController = Depends(Factory().get_user_controller),
+):
+    return await user_controller.update_user(current_user.id, user_data)
 
 
-@profile_router.put("/password")
-async def update_password(user: User = Depends(get_current_user)):
-    return {"message": "User password updated", "user": user}
+@profile_router.patch("/")
+async def update_profile(
+    user_data: ProfilePartialUpdateRequest,
+    current_user: User = Depends(get_current_user),
+    user_controller: UserController = Depends(Factory().get_user_controller),
+):
+    return await user_controller.update_user(
+        current_user.id, user_data, partial_update=True
+    )
